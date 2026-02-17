9df97123e69fb0ecb5fc7fdf1034afdb48cfca30
Manuel Barros <manuelqmbarros@gmail.com>
2026-02-17 13:28:45 -0500

    update analysis

---
+++ analysis/candidate_correlation.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import preprocessing as pre
+from domain import Problem
+
+time_color = "#9467bd"
+cand_color = "#2ca02c"
+
+# Load your data
+df = pd.read_csv("../isolde-experiments/data/80b8403f476d5b.csv")
+
+df = pre.preprocess(
+    df,
+    txn_num=(3, 10),
+    problems=[Problem.from_str("SI_ax_c\tSI_ax_b")],
+    implementations=["all"],
+)
+
+# Extract relevant columns
+txn = df["num_txn"]
+time_mean = df["avg_time_ms"]
+time_min = df["min_time_ms"]
+time_max = df["max_time_ms"]
+candidates = df["candidates"]
+
+# Calculate the error bars for time and candidates
+time_error_low = time_mean - time_min
+time_error_high = time_max - time_mean
+
+# Create the figure and first axis
+fig, ax1 = plt.subplots(figsize=(5, 3))
+
+# Plot 'time' on the left y-axis with error bars
+ax1.errorbar(
+    txn,
+    time_mean,
+    yerr=[time_error_low, time_error_high],
+    fmt="-^",
+    color=time_color,
+    label="Time",
+)
+ax1.set_xlabel("Number of transactions")
+ax1.set_ylabel("Time (ms)")
+ax1.tick_params(axis="y")
+
+# Create a second y-axis sharing the same x-axis
+ax2 = ax1.twinx()
+# Plot 'candidates' on the right y-axis with error bars
+ax2.errorbar(txn, candidates, color=cand_color, fmt=":.", label="Candidates")
+ax2.set_ylabel("Candidates")
+ax2.tick_params(axis="y")
+
+ax1.grid(
+    True,  # turn grid on
+    which="both",  # 'major', 'minor', or 'both'
+    axis="both",  # 'x', 'y', or 'both'
+    linestyle="--",  # e.g. '-', '--', ':', '-.'
+    linewidth=0.5,
+    color="gray",
+    alpha=0.7,
+)
+# Add a legend
+fig.legend(loc="upper left", bbox_to_anchor=(0.31, 0.95))
+plt.tight_layout()
+
+plt.savefig("plots/time_candidate.pdf", format="pdf")
--- analysis/vldb.py
+++ analysis/vldb.py
@@ -1,25 +1,94 @@
-import itertools
-from typing import List
+Class = tuple[str, int]
 
-from domain import Definition, Problem
-from plotting import Style
-from preprocessing import merge_rows
+TableData = dict[str, dict[str, dict[int, tuple[tuple[int, int], tuple[int, int]]]]]
+Helper = dict[str, dict[str, int]]
+
+timeout = 3600000
+
+def dfToTableData(df, txn_lim=10) -> TableData:
+    d = {}
+    helper = {}
+    for _, row in df.iterrows():
+        implementation = row["implementation"]
+        num_txn = row["num_txn"]
+        expected = row["expected"]
+        avg_time_ms = row["avg_time_ms"]
+        frameworks = row["frameworks"]
+        candidates = row["candidates"]
+
+        if implementation not in d:
+            d[implementation] = {}
+            helper[implementation] = {}
+        problem_class = (expected, frameworks)
+        if problem_class not in d[implementation]:
+            d[implementation][problem_class] = {}
+        if num_txn not in d[implementation][problem_class]:
+            d[implementation][problem_class][num_txn] = (
+                (avg_time_ms, candidates),
+                (avg_time_ms, candidates),
+            )
+
+        (min_time, min_cand), (max_time, max_cand) = d[implementation][problem_class][
+            num_txn
+        ]
+
+        if avg_time_ms < min_time:
+            min_time, min_cand = avg_time_ms, candidates
+        if avg_time_ms > max_time:
+            max_time, max_cand = avg_time_ms, candidates
+
+        d[implementation][problem_class][num_txn] = (
+            (min_time, min_cand),
+            (max_time, max_cand),
+        )
+
+        if avg_time_ms == timeout:
+            for i in range(num_txn + 1, txn_lim + 1):
+                if i in d[implementation][problem_class]:
+                    min_time = d[implementation][problem_class][i][0]
+                else:
+                    min_time = (timeout + 1000, -1)
+                d[implementation][problem_class][i] = (min_time, (avg_time_ms, -1))
+    return d
 
 
-def clean_rq1(df, setup, txn_max_lim=None):
-    df = merge_rows(
-        df,
-        setup=setup,
-        txn_max_lim=txn_max_lim,
-        check_num_measurements=False,
-        implementations=["default"],
-    )
-    df["sat"] = df["problem"].apply(lambda p: "SAT" if sat(p) else "UNSAT")
-    return df
+def formatTable2(data: TableData, num_txn=5) -> str:
+    s = ""
+    for impl, impl_data in data.items():
+        s += r"\midrule" + "\n" + r"\multirow{2}{*}{" + impl.replace("_", " ") + r"}"
+        for pclass, pclass_data in sorted(impl_data.items()):
+            (_, (time, _)) = pclass_data[num_txn]
+            s += f" & {time if time < timeout else "\\multirow{2}{*}{TO}"}"
+        s += r"\\" + "\n\n"
+        for pclass, pclass_data in sorted(impl_data.items()):
+            (_, (time, cand)) = pclass_data[num_txn]
+            s += f" & {cand if time < timeout else ""}"
+        s += r"\\" + "\n\n"
+    return s
 
 
-def clean_rq2(df, setup, txn_max_lim=None):
-    df = clean_rq1(df, setup=setup, txn_max_lim=txn_max_lim)
-    df["optimizations"] = df["implementation"].apply(optimizations)
-    df = df[df["problem"].isin(problems_rq1[2:])]
-    return df
+table2Header = r"""\toprule
+                    & SAT single fw & SAT diff fws & UNSAT single fw & UNSAT diff fws \\
+"""
+
+
+def formatTable3(data: TableData) -> str:
+    s = ""
+    for impl, impl_data in data.items():
+        s += r"\midrule" + "\n" + r"\multirow{2}{*}{" + impl.replace("_", " ") + r"}"
+        for pclass, pclass_data in sorted(impl_data.items()):
+            for num_txn, (_, (time, cand)) in pclass_data.items():
+                s += f" & {time if time < timeout else "\\multirow{2}{*}{TO}"}"
+        s += r"\\" + "\n"
+        for pclass, pclass_data in sorted(impl_data.items()):
+            for num_txn, (_, (time, cand)) in pclass_data.items():
+                s += f" & {cand if time < timeout else ""}"
+        s += r"\\" + "\n\n"
+    return s
+
+
+table3Header = r"""\toprule
+                    & \multicolumn{3}{c}{SAT single fw} & \multicolumn{3}{c}{SAT diff fws} & \multicolumn{3}{c}{UNSAT single fw} & \multicolumn{3}{c}{UNSAT diff fws} \\
+		\cmidrule(lr){2-13}
+                    & 3 & 4 & 5 & 3 & 4 & 5 & 3 & 4 & 5  & 3 & 4 & 5 \\  
+"""
